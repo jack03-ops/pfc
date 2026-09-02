773b35b5beebe3db9410778800db269b29cdb2a1
Hari Ram <hariramkumar.a2001@gmail.com>
2026-09-02 21:58:51 +0530

    style: remove top header bar to streamline layout and eliminate title duplication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Sidebar from './components/Sidebar';
-import Header from './components/Header';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import MembersList from './pages/MembersList';
@@ -251,13 +250,6 @@ export default function App() {
 
       {/* Main Container Content */}
       <div className="flex-1 flex flex-col min-w-0 w-full overflow-x-hidden">
-        {/* Global header bar */}
-        <Header 
-          title={pageTitles[currentPage] || 'Phoenix System'} 
-          user={user} 
-          setPage={setCurrentPage}
-        />
-        
         {/* Page content window */}
         <main className="flex-1 overflow-x-hidden overflow-y-auto">
           {renderPage()}
